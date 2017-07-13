bb92adc943eb0c042e536e950d05e5005b58f6ad
Daniel Bucket <danieljbucket@gmail.com>
2017-07-13 17:43:15 -0600

    started testing for MovieCard

--- app/components/MovieCard/MovieCard.test.js
+++ app/components/MovieCard/MovieCard.test.js
@@ -4,10 +4,30 @@ import MovieCard          from './MovieCard';
 import ReactDOM           from 'react-dom';
 import { Provider }       from 'react-redux';
 import configureMockStore from 'redux-mock-store';
+import { getImageURL } from '../../utils/constants';
+
 
 describe('MovieCard', () => {
   it('should', () => {
 
+    const movie = {
+      "id": 334521,
+      "title": "Free Fire",
+      "overview": "A crime drama set in 1970s Boston, about a gun sale which goes wrong.",
+      "releaseDate": "2017-03-31",
+      "poster": "https://image.tmdb.org/t/p/w500//dbKsjUlJCIE7wYueq0K6y4EdW0A.jpg",
+      "backdrop": "https://image.tmdb.org/t/p/w500//le2X3o9uopj1jMcMKFDKQ7MdBxb.jpg"
+    }
+
+    const wrapper = shallow(<MovieCard movie={movie}/>)
+
+    expect(MovieCard({movie}).props.children.length).toEqual(2)
+
+    console.log(wrapper.find('.movie-info')  )
+
+
+    // expect(MovieCard({movie}).props.children[0].props.className).toEqual('sub-card')
+    // expect(MovieCard({movie}).props.children[1].props.children).toEqual('Title')
   })
 
   it('should', () => {
--- app/model/Movie.test.js
+++ app/model/Movie.test.js
@@ -1,12 +1,16 @@
 import Movie from './Movie';
+// import { getImageURL } from '../utils/constants';
 
 describe('Movie', () => {
   it('should', () => {
+    const expected = {"backdrop": "https://image.tmdb.org/t/p/w500/undefined",
+                      "id": undefined,
+                      "overview": undefined,
+                      "poster": "https://image.tmdb.org/t/p/w500/undefined",
+                      "releaseDate": undefined,
+                      "title": undefined
+                     }
 
-  }
-
-  it('should', () => {
-
-  }
-
-}
+    expect(new Movie({expected})).toEqual(expected)
+  })
+})
