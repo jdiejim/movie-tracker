0598ab6936116d12c1e6c42ceb4def16ecf97603
Kristi Green <kngreen40@gmail.com>
2017-07-15 17:05:45 -0600

    work on signin and login validation

--- app/components/LogInPopUp/LogInPopUp.js
+++ app/components/LogInPopUp/LogInPopUp.js
@@ -7,12 +7,15 @@ class LogInPopUp extends Component {
       name: '',
       email: '',
       password: '',
+      disabled: true
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
+    this.handleDisabled = this.handleDisabled.bind(this);
   }
+
 
   componentWillReceiveProps(np) {
     console.log('np', np);
@@ -27,6 +30,7 @@ class LogInPopUp extends Component {
     const input = e.target.value;
     const key = e.target.name;
     this.setState({[key]: input})
+    this.handleDisabled()
   }
 
   handleSubmit(e) {
@@ -38,8 +42,31 @@ class LogInPopUp extends Component {
       logIn(JSON.stringify(this.state))
     }
     this.setState({name: '', email: '', password: ''} )
+  }
 
-
+  handleDisabled() {
+    const { type } = this.props;
+    let { disabled, name, email, password } = this.state
+    console.log(this.state);
+    switch(type) {
+      case 'login':
+        if ( email !== '' && password !== '') {
+          disabled = false;
+        } else {
+          disabled = true;
+        }
+      case 'signup':
+        if ( name === '' && email === '' && password === '' ) {
+          disabled = true;
+        } else {
+          disabled = false;
+        }
+      // default:
+        // disabled = 'Im here';
+    }
+    console.log(email);
+    console.log(disabled);
+    this.setState({disabled})
   }
 
   handleBlur() {
@@ -49,10 +76,10 @@ class LogInPopUp extends Component {
 
 
   render() {
-    const  { name, email, password } = this.state
+    const  { name, email, password, disabled } = this.state
     const { type, signUp, userFail } = this.props;
     const title = type === 'signup' ? 'Sign Up' : 'Log In'
-    const nameInput = type === 'signup' ? <input className='popup-input' onChange={ this.handleChange } type="text" name="name" placeholder="Enter name" value={name} /> : '';
+    const nameInput = type === 'signup' ? <input id="name" className='popup-input' onChange={ this.handleChange } type="text" name="name" placeholder="Enter name" value={name} /> : '';
     const errorMsg = userFail ? <p className="login-error-msg">Email or password not valid</p> : null;
 
     return (
@@ -62,11 +89,11 @@ class LogInPopUp extends Component {
         <form className='login-popup' onSubmit={ this.handleSubmit }>
           { nameInput }
           <section className="popup-input-wrapper">
-            <input className='popup-input' onChange={ this.handleChange } type="text" name="email" placeholder="Enter email" value={email} />
-            <input className='popup-input' onChange={ this.handleChange } type="text" name="password" placeholder="Enter password" value={password} />
+            <input ref={(input) => this.emailInput = input} id="email" className='popup-input' onChange={ this.handleChange } type="text" name="email" placeholder="Enter email" value={email} />
+            <input id="password" className='popup-input' onChange={ this.handleChange } type="text" name="password" placeholder="Enter password" value={password} />
           </section>
           {errorMsg}
-          <button className='popup-login-btn' type="submit">{ title }</button>
+          <button className='popup-login-btn' type="submit" disabled={disabled}>{ title }</button>
         </form>
       </div>
     )
