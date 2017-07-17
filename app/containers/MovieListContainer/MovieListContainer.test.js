d3bbdefc09f5c0301d9bcf235a092b5d7c04fe40
coleworsley <cole.s.worsley@gmail.com>
2017-07-16 18:38:11 -0600

    add tests for movie list and app

--- app/containers/MovieListContainer/MovieListContainer.test.js
+++ app/containers/MovieListContainer/MovieListContainer.test.js
@@ -1,6 +1,10 @@
 import MovieListContainer       from './MovieListContainer';
+import MovieList                from '../../components/MovieList/MovieList';
 import { API_KEY }              from '../../utils/api_key'
-import { movieDataStub, user }  from '../../utils/stubs';
+import {
+  movieDataStub,
+  user,
+  favorites }  from '../../utils/stubs';
 
 import React                    from 'react';
 import { mount }                from 'enzyme';
@@ -14,7 +18,15 @@ import thunk                    from 'redux-thunk';
 
 
 const history = createHistory()
-const store   = configureMockStore([thunk])({movies:movieDataStub.results, user:user})
+const loggedInStore = configureMockStore([thunk])({
+  movies: movieDataStub.results,
+  user: user,
+  favorites})
+
+const loggedOutStore = configureMockStore([thunk])({
+  movies: movieDataStub.results,
+  user: {},
+  favorites: []})
 
 
 describe('MovieListContainer', () => {
@@ -23,6 +35,11 @@ describe('MovieListContainer', () => {
     fetchMock.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`, {
       status: 200,
       body: movieDataStub,
+    })
+
+    fetchMock.get(`/api/users/1/favorites`, {
+      status: 200,
+      body: favorites,
     })
   })
 
@@ -42,7 +59,7 @@ describe('MovieListContainer', () => {
 
   it('should mount', () => {
     const wrapper = mount(
-          <Provider store={store} >
+          <Provider store={loggedOutStore} >
             <ConnectedRouter history={history} >
               <Route to='' history={history} component={MovieListContainer}/>
             </ConnectedRouter>
@@ -52,9 +69,9 @@ describe('MovieListContainer', () => {
     expect(wrapper.find('.movie-list').length).toEqual(1)
   })
 
-  it.only('should render movie cards', () => {
+  it('should render movie cards', () => {
     const wrapper = mount(
-          <Provider store={store} >
+          <Provider store={loggedOutStore} >
             <ConnectedRouter history={history} >
               <Route to='' history={history} component={MovieListContainer}/>
             </ConnectedRouter>
@@ -64,35 +81,48 @@ describe('MovieListContainer', () => {
     expect(wrapper.find('.movie-card').length).toEqual(2)
   })
 
-  it.only('should have props passed in', () => {
+  it('should have props passed in', () => {
     const wrapper = mount(
-          <Provider store={store} >
+          <Provider store={loggedOutStore} >
             <ConnectedRouter history={history} >
-              <Route to='' history={history} component={MovieListContainer}/>
+              <Route to='/' history={history} component={MovieListContainer}/>
             </ConnectedRouter>
           </Provider>
     )
-    console.log(wrapper.props())
-    // expect(wrapper.find('.movie-card').length).toEqual(2)
+
+    const movieListComponent = wrapper.find('MovieList')
+
+    expect(movieListComponent.prop('movies').length).toEqual(2)
+    expect(movieListComponent.prop('favorites').length).toEqual(0)
+    expect(movieListComponent.prop('user')).toEqual({})
   })
 
-  it('should redirect to login when the favorites button is clicked and the user is not logged in', () => {
+  it('should display a save button when the user is not logged in or has not favorited the movies', () => {
     const wrapper = mount(
-          <Provider store={store} >
+          <Provider store={loggedOutStore} >
             <ConnectedRouter history={history} >
               <Route to='' history={history} component={MovieListContainer}/>
             </ConnectedRouter>
           </Provider>
     )
+
+    const links = wrapper.find('.save-btn');
+    links.forEach(node => {
+      expect(node.text()).toBe('Save')
+    })
   })
 
-  it('should be able to add a movie to favorites', () => {
+  it('should be able to add a movie to favorites when logged in', () => {
     const wrapper = mount(
-          <Provider store={store} >
+          <Provider store={loggedInStore} >
             <ConnectedRouter history={history} >
               <Route to='' history={history} component={MovieListContainer}/>
             </ConnectedRouter>
           </Provider>
     )
+
+    const links = wrapper.find('.save-btn');
+    expect(links.at(0).text()).toBe('Remove')
+    expect(links.at(1).text()).toBe('Save')
   })
 })
